84eac0ed41adc8fb5c0d16b8a33bb1ab5c41c6e6
Abdullah Erdoğan <74793654+abdullaherdogan@users.noreply.github.com>
2021-11-13 16:24:46 +0300

    enviroment updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyD-Rbpd2-ECyrkBkNx-aZnLHBByPLpqoh4",
-    authDomain: "kitap-satis-site.firebaseapp.com",
-    projectId: "kitap-satis-site",
-    storageBucket: "kitap-satis-site.appspot.com",
-    messagingSenderId: "915356389762",
-    appId: "1:915356389762:web:9064368eb35d378c12ff9b"
+    apiKey: "xxxx",
+    authDomain: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
   }
 };
